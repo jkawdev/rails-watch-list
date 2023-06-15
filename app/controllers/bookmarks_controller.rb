edaac25dc09ad9253dd3d55a8013cefb4b618e85
VincentBeaumont <beaumontvincent@yahoo.fr>
2023-06-15 23:22:59 +0200

    watch_list_signets_almost

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,31 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+    @movies = Movie.all
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    # IL FAUT ALLER CHERCHER LE MOVIE_id POUR VALIDER LE FORMULAIRE --> Correspond à f.association ds le formulaire
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
